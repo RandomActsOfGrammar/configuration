1735717964146e147d7e1b1dd1b043c41da7bfe9
~ <demichaelson@gmail.com>
2018-12-12 10:59:26 -0600

    Getting keybindings working for moving the frame up and down

--- emacs.el
+++ emacs.el
@@ -73,8 +73,8 @@
 (global-set-key (kbd "<C-down>") 'shrink-window)
 
 ;;move the view up or down one line
-(global-set-key (kbd "<C-j>") 'scroll-down-line)
-(global-set-key (kbd "<C-o>") 'scroll-up-line)
+(global-set-key (kbd "C-o") 'scroll-down-line)
+(global-set-key (kbd "C-j") 'scroll-up-line)
 
 ;;Limit width of lines in text mode--automatically shortens when you hit
 ;;    <enter> or do a <space>
