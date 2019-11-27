342f9bb96f83874eda1ad2949c9258d912327367
~ <demichaelson@gmail.com>
2019-11-27 08:59:27 -0600

    Making Proof General always use the right layout

--- emacs.el
+++ emacs.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(c-basic-offset 4))
+ '(c-basic-offset 4)
+ '(proof-three-window-mode-policy (quote hybrid)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
