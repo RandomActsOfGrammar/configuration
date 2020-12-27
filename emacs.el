8d15b46987c6caf7ea8501f5f8899c1e45f62b5a
~ <demichaelson@gmail.com>
2020-12-27 16:25:37 -0600

    Adding better syntax highlighting for Abella in Emacs

--- emacs.el
+++ emacs.el
@@ -165,7 +165,31 @@
     ("Subgoal \\(\\([0-9]+\\.*\\)+\\) is" 1 font-lock-type-face)
     ("Subgoal .* \\(is\\)" 1 font-lock-keyword-face)
     ("\\([-A-Za-z^=`'?$0-9_*@+#!~\/]+\\) :" 1 font-lock-type-face)
-    ("Proof completed." . font-lock-string-face)))
+    ("Proof completed." . font-lock-string-face)
+    ;;trying to highlight variable names in declaration spots
+    ("Variables: *\\(\\([-A-Za-z^=`'?$0-9_*@+#!~\/]+ *\\)+\\)" 1 font-lock-variable-name-face)
+    ("\\(\\(forall\\)\\|\\(exists\\)\\) +\\(\\([-A-Za-z^=`'?$0-9_*@+#!~\/]+ *\\)+ *\\)," 4 font-lock-variable-name-face)
+    )
+  )
+
+;;I'd like some of this highlighting in regular Abella mode as well
+(font-lock-add-keywords 'abella-mode
+  ;;Highlighting variable declarations
+    ;;It turns out we another mechanism if we want to highlight the types in declarations and not have all variables there typed
+  '(("\\(\\(forall\\)\\|\\(exists\\)\\|\\(nabla\\)\\) +\\(\\(\\([-A-Za-z^=`'?$0-9_*@+#!~\/]+ *\\)\\|\\((\\([-A-Za-z^=`'?$0-9_*@+#!~\/]+ *\\)+: *\\([^,\\.]+\\))\\) *\\)+\\) *," 5 font-lock-variable-name-face) ;;all variables bound at a quantifier
+    ("(\\([-A-Za-z^=`'?$0-9_*@+#!~\/]+ *\\)+ *: *\\([^,\\.]+\\))" 2 font-lock-type-face) ;;types declared with variables
+    ("(\\(\\([-A-Za-z^=`'?$0-9_*@+#!~\/]+ *\\)+\\) *: *\\([^,\\.]+\\))" 1 font-lock-variable-name-face) ;;variables declared (or used) with types
+  ;;Highlighting types for kind, type, and defines
+    ("Kind +\\(\\([-A-Za-z^=`'?$0-9_*@+#!~\/]+ *, *\\)*[-A-Za-z^=`'?$0-9_*@+#!~\/]+\\) +[^\\.]+ *." 1 font-lock-type-face) ;;declaring the kind of a type
+    ("Type +\\([-A-Za-z^=`'?$0-9_*@+#!~\/]+ *, *\\)*[-A-Za-z^=`'?$0-9_*@+#!~\/]+ +\\([^\\.]+\\) *." 2 font-lock-type-face) ;;declaring the type of a constructor
+    ("[-A-Za-z^=`'?$0-9_*@+#!~\/]+ *: *\\([^\n,]+\\) *," 1 font-lock-type-face) ;;early relation in a Define
+    ("[-A-Za-z^=`'?$0-9_*@+#!~\/]+ *: *\\([^\n,]+\\) +by" 1 font-lock-type-face) ;;final relation in a Define
+  ;;Highlighting name for type and defines
+    ("Type +\\(\\([-A-Za-z^=`'?$0-9_*@+#!~\/]+ *, *\\)*[-A-Za-z^=`'?$0-9_*@+#!~\/]+\\) +\\([^\\.]+\\) *." 1 font-lock-variable-name-face) ;;declaring the type of a constructor
+    ("\\([-A-Za-z^=`'?$0-9_*@+#!~\/]+\\) *: *\\([^\n,]+\\) *," 1 font-lock-variable-name-face) ;;early relation in a Define
+    ("\\([-A-Za-z^=`'?$0-9_*@+#!~\/]+\\) *: *\\([^\n,]+\\) +by" 1 font-lock-variable-name-face) ;;final relation in a Define
+    )
+  )
 
 ;;OCaml mode
 (load
